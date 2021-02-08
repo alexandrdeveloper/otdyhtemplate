1dd4ee266897df79f4c3dff01f5f136ef842442c
Alexandr <33997516+alexwebdev96@users.noreply.github.com>
2021-02-08 23:54:12 +0200

    First upload

---
+++ js/common.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", function() {
+	
+/* Home Page Scripts */   
+
+    for (const dropdown of document.querySelectorAll(".b-select-group")) {
+        dropdown.addEventListener('click', function() {
+            this.querySelector('.custom-select').classList.toggle('custom-select_open');
+        });
+        for (const option of document.querySelectorAll(".custom-options__item")) {
+            option.addEventListener('click', function() {
+                if (!this.classList.contains('custom-options__item_selected')) {
+                    this.parentNode.querySelector('.custom-options__item_selected').classList.remove('custom-options__item_selected');
+                    this.classList.add('custom-options__item_selected');
+                    this.closest('.custom-select').querySelector('.custom-select__trigger span').textContent = this.textContent;
+                }
+            })
+        }
+    }
+
+    window.addEventListener('click', function(e) {
+        for (const select of document.querySelectorAll('.custom-select')) {
+            if (!select.contains(e.target)) {
+                select.classList.remove('custom-select_open');
+            }
+        }
+    });
+    
+/* Objects Page Scripts */ 
+    for (const modalToggle of document.querySelectorAll(".modal-toggle")) {
+        modalToggle.addEventListener('click', function(e) {
+            e.preventDefault();
+            document.querySelector('.b-modal').classList.toggle('b-modal_visible');
+            document.querySelector('body').classList.toggle('no-scroll');
+
+        })
+    }
+    for (const modalToggle of document.querySelectorAll(".b-modal-close")) {
+        modalToggle.addEventListener('click', function(e) {
+            e.preventDefault();
+            document.querySelector('.b-modal').classList.remove('b-modal_visible');
+            document.querySelector('body').classList.remove('no-scroll');
+        })
+    }
+
+   window.addEventListener('click', function(e) {
+        for (const popupWindow of document.querySelectorAll('.b-modal-window')) {
+            if (!popup.contains(e.target) && document.querySelector('.b-modal').classList.contains('b-modal_visible')) {
+                document.querySelector('.b-modal').classList.remove('b-modal_visible');
+            }
+        }
+    })
+    
+
+});
+	
+
+
